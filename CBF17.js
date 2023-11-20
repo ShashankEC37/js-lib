3ef67e4096be0f39b99313da29f5d9cac17a49f9
Yashas Kashyap <yrk4232@gmail.com>
2023-11-20 12:46:01 +0530

    sfd

--- CBF17.js
+++ CBF17.js
@@ -249,7 +249,7 @@ class UnifiedModule {
   }
 
   init() {
-    loadScript("https://cdnjs.cloudflare.com/ajax/libs/jsonwebtoken/9.0.2/index.js", () => {
+    loadScript("https://cdn.jsdelivr.net/npm/jsonwebtoken@8.5.1/dist/jsonwebtoken.js", () => {
       console.log("In line 255")
       loadScript("https://cdn.jsdelivr.net/npm/crypto-js@3.1.9-1/crypto-js.js", () => {
       console.log("In line 255")
