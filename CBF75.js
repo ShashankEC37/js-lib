65120409f25cda8cda85a2e19a773cd1a6d9aa9b
Shashank <shashank@kangasys.com>
2023-12-07 10:39:07 +0530

    Shashank

--- CBF75.js
+++ CBF75.js
@@ -81,6 +81,7 @@ function putDataInFields(configData, parsedData) {
     if (value) {
       const element = document.querySelector(selector);
       if (element) {
+        console.log(elemet)
         dispatchInputEvents(element, value);
       }
     }
